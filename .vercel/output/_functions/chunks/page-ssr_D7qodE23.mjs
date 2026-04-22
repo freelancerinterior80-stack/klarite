3b5d3b164ceb6419e1e9401224311d60cf76438f
bainterior20-rgb <bainterior20@gmail.com>
2026-04-22 19:43:02 +0300

    fix: remove committed .vercel build artifacts, add to gitignore

--- .vercel/output/_functions/chunks/page-ssr_D7qodE23.mjs
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@sanity/client';
-
-const sanityClient = createClient(
-            {"apiVersion":"v2023-08-24","projectId":"zi14m3a6","dataset":"production","useCdn":true}
-          );
-
-globalThis.sanityClient = sanityClient;
--- .vercel/output/static/_astro/studio-component.Ctx-WoYT.js
+++
@@ -1,1 +0,0 @@
-import{ar as t,as as n}from"./pane2.momfo7_0.js";export{t as StudioComponent,n as StudioComponentHash};
